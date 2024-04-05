275623f00d46a6a55bbb0314911eae07c75a53f0
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2024-04-05 19:04:23 +0530

    🎨 Footer styling #2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,35 +5,37 @@ const Footer = ({ toggleTheme }) => {
   return (
     <footer>
       <div className="footer-content">
-        <div className="left">
-          <span>
-            © {new Date().getFullYear()} | Developed with{" "}
-            <i className="fa-solid fa-heart"></i> by{" "}
-            <a
-              target="_blank"
-              className="link"
-              href="https://github.com/SahilK-027"
-            >
-              SahilK-027
-            </a>
-          </span>
-        </div>
+        <div className="page">
+          <div className="left">
+            <span>
+              © {new Date().getFullYear()} | Developed with{" "}
+              <i className="fa-solid fa-heart"></i> by{" "}
+              <a
+                target="_blank"
+                className="link"
+                href="https://github.com/SahilK-027"
+              >
+                SahilK-027
+              </a>
+            </span>
+          </div>
 
-        <div className="right">
-          Theme
-          <span>
-            <input
-              type="checkbox"
-              className="checkbox"
-              id="checkbox"
-              onChange={toggleTheme}
-            />
-            <label htmlFor="checkbox" className="checkbox-label">
-              <i className="fas fa-moon"></i>
-              <i className="fas fa-sun"></i>
-              <span className="ball"></span>
-            </label>
-          </span>
+          <div className="right">
+            Theme
+            <span>
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="checkbox"
+                onChange={toggleTheme}
+              />
+              <label htmlFor="checkbox" className="checkbox-label">
+                <i className="fas fa-moon"></i>
+                <i className="fas fa-sun"></i>
+                <span className="ball"></span>
+              </label>
+            </span>
+          </div>
         </div>
       </div>
     </footer>

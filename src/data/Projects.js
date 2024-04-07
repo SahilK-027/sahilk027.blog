9ff3ee6506821ee741e2c6cda138a2773ed4a806
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2024-04-07 10:55:59 +0530

    🐞 fix broken project thumbnails

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,10 +1,19 @@
+import tr2023 from "../assets/projectThumbnails/trinity-2023.webp";
+import innovision from "../assets/projectThumbnails/innovision.webp";
+import giticon from "../assets/projectThumbnails/giticon.webp";
+import butterfly from "../assets/projectThumbnails/butterfly.webp";
+import assemblescript from "../assets/projectThumbnails/assemblescript.webp";
+import sight from "../assets/projectThumbnails/sight.webp";
+import tr2024 from "../assets/projectThumbnails/trinity-2024.webp";
+import particlesGPGPU from "../assets/projectThumbnails/particles-gpgpu.webp";
+import ironmanHolograms from "../assets/projectThumbnails/ironman-holograms.webp";
+
 export const Projects = [
   {
     id: 1,
     idSelector: "trinity-2023",
     name: "Trinity 2023",
-    thumbnailUrl:
-      "https://ucba91ba7a4b96d9d0a2fde37d73.previews.dropboxusercontent.com/p/thumb/ACM_sPbcGaJ1_lPvKk1QlylOUJqV1NyAgoacyi0rqhjOi1Uue5A3JaVb1PPRAF2gwXVIhRPuddB2CYiAcnHGtWv7xG6Ue509t9zy_iagZa9m51jrw4sCRj0-m3XUAHSbxqsOxU8hdAQahgAA9ZcYUsRCV3KN4Qs_uY65dLyPceEXuuSfmBxt3YusshW9h6Vf3KT2N0k1vQSMDHQuS7ikCBmi-By6fObKYyj0IEoudtF-g-dq3aqUknR_3IbuvCqUHFsA9LoakVSLkQF2G7TuQXYbdjVfPe3upyyiijmQzxzYDPHJzpjiQtgUfllilKzfBPeJn0dF6unbLjoDX24n52cT_wUOsBRxBY8Q2fXnsrxYFakZGjjcEJ2JrjWhK44RC7I/p.png",
+    thumbnailUrl: tr2023,
     concept: ["3D", "Web", "Full Stack"],
     projectUrl: "/projects/trinity-2023",
     // Detailed Description
@@ -93,8 +102,7 @@ export const Projects = [
     id: 2,
     idSelector: "innovision",
     name: "Innovision",
-    thumbnailUrl:
-      "https://uca30a52db3871e7e625f8c5bfc7.previews.dropboxusercontent.com/p/thumb/ACNdfLrTeFQ62Gpp1iPTvzzs1BSEsarpzY_TPQur7x7qLyrrteZcQ9nisPu1sc6hGNBMsr5n2zOnwlIcl5zlvLHTr503AwknwvQV4LDgjmuPgCRa9ce7hTRaqASqOlFrGJxgMXmhb2qvB-VifAryH0fWMju_RFux2fKIEBr7b382DUhVBtqKvW63dRGVX0-5DMg_P0HlggBnB3MclglCK3zcYykd5tjubGfel0YKS4e537ylAeNTkQ_YmessGztHvAWJZL6vnZB46_TKHiWEwX4v1BB_dpSoMOMZ7pkieUOIl-dB2-3ZVY7bb9HeeBJqhoMarqWlshHp-mUNJXpOJ8wWj7ZBd0oQp_amzTMDtu3x1dKgpxpzwVKSsiGp6OICOlo/p.png",
+    thumbnailUrl: innovision,
     concept: ["3D", "Web", "Frontend"],
     projectUrl: "/projects/innovision",
   },
@@ -102,8 +110,7 @@ export const Projects = [
     id: 3,
     idSelector: "giticon",
     name: "Giticon",
-    thumbnailUrl:
-      "https://uc04dac08fdf3240fd7954ceabfb.previews.dropboxusercontent.com/p/thumb/ACPOUVEfE3Hk3WBZxnKt43-5o9q378bNBBVxK4O-QzTR2E7nFQsHbpIlG4sSAgIfL42JJUYwQWTkQNOZBR7hFeubPIzrTWsXiAGJUi2WzS7WWAnrjbalo_y38qHLGD0GK9ruCVbzZpRBChDoVAbUbBPxvhQvSTmXagrwKJp5XN4La98k8eDHeTs6UT8LoePUC1n_WBL6mS23cIzXRRSQwOJ-qXbFTGeHFM_fDZj8mwsKPuymAXm9OciSNIPzdiozFc40h8ru2dv7liHmNnRylSisY3Ogm_SzVJS6deYsURe7M1xxhQoOfRUfd7LLi285l0VmGNPpXuHahPvU0RLEauitICezadW0z0of7VjM9RAV8YHSGSnYUequ_V2-UtZdDhI/p.png",
+    thumbnailUrl: giticon,
     concept: ["3D", "Web", "Frontend"],
     projectUrl: "/projects/giticon",
   },
@@ -111,8 +118,7 @@ export const Projects = [
     id: 4,
     idSelector: "butterfly-experience",
     name: "Butterfly",
-    thumbnailUrl:
-      "https://uc73666877a1fc11c5695d8a39b6.previews.dropboxusercontent.com/p/thumb/ACMBO5ei9GCdLGvnUgGimRO5XtDMFk4RnvMRX0icJ9v-5BnDI-jobU_BPEu3lpT4hbWAHPPk6JlwsfyBAgACCCZOSMOjRneK8eOW0gs8Such3LOgjbbpEaksotpzLshVYwUn23qtmjx1tjzKx82tQfGL566Wx7z4SVSmIWiN1WsawhXZMeoXnD3r2c1fEHkdtaH20GzimB2kacw3UPAMgC3sZOJsk-qfkVjSZIC7lH5_TKZ64dpxsmV8-NjyVPCBQFJCFqOxF7M8n_eVW9rTmj44zSir71uAc11dLpWIez78ylnxYnVREuSIlosp1UmT0w101MLyF4fK4kWuoXWgHCgr-ZuEh-NkaOm2WJyJLvg8S8BCgsVtI2xdNGDpp-bYpi4/p.png",
+    thumbnailUrl: butterfly,
     concept: ["3D Illustration", "Digital Art"],
     projectUrl: "/projects/butterfly-experience",
   },
@@ -120,8 +126,7 @@ export const Projects = [
     id: 5,
     idSelector: "assemblescript",
     name: "AssembleScript",
-    thumbnailUrl:
-      "https://ucd807e576685c223c9593445abd.previews.dropboxusercontent.com/p/thumb/ACMxpd3oKUXNPpt32Z0gPa_P0sgwVrJvSIzI8u9W4L5dhJ3G3kHiYoRnU19CMlHAqAlS8nbJEin1_zIdQp7S9yR5hRCCgudipIH4YyfkRQ32RfVJbaUfoKatekeFNsO88L9nU8iw-kYiuqmfwLbKlNrHRHfT7NpMltsklI-UE9K317kKiONxh_svbntSCciMiGVQ3T3tuUPhQJVwK5r07OZdw43sss98jJhyZi0XGdCLkxn3rvNjAh1tEs3ewLeyC5leLudugYrl3Z56ztaobLHJ9lPFrg1CyV12aT_lMmfFDpzuQoAsnJY313xo1ZnnlRzEIjBc6MPa-LPbyjgfaPliit3LTZweQuDk7ODru_kluLAH0dpRlp4mlLuraqqNWpk/p.png",
+    thumbnailUrl: assemblescript,
     concept: ["3D", "Web", "Full Stack", "Compiler"],
     projectUrl: "/projects/assemblescript",
   },
@@ -129,8 +134,7 @@ export const Projects = [
     id: 6,
     idSelector: "sight",
     name: "SIGHT Analyzer",
-    thumbnailUrl:
-      "https://uc0e992ebbe973cba7b9b2f6080b.previews.dropboxusercontent.com/p/thumb/ACMz-G0vvQC83kEHN0hjNtZON5vFEEzGVkZIdUPpfHl1B0WuzSy4aH0JsZhtFd1k9213tix8gG-T8IGoKHwr8yiHhBBHN_FJfgBeXYtqqdOuudvaP-DtS1OGH9gEKjTGchlaDN6xlL4HB2zZC4ZfB6KCTAxz2cyIheFaAPR7S7LSK1Wic9Xk6cDAHWLPNUvjzWnC9wTy6OucXwou0zYNTD33cGNyheQSprlO8ZJ_1ig8bFip2OA2SGvWHb25eGQcVuQIpoDvug2ZO3q5QAnpwwXj8xUSDvdiWGUmc8w9MHD_L_zP18OTE1OeLpe5htr4aATmNuu-1fabsGrP8k5qJyON1mpLb4qP7g2jv49kslvyJSCS1gQ2SGUxxtyVlOcgPfc/p.png",
+    thumbnailUrl: sight,
     concept: ["ML", "OpenCV", "YoloV8"],
     projectUrl: "/projects/sight",
   },
@@ -138,8 +142,7 @@ export const Projects = [
     id: 7,
     idSelector: "trinity-2024",
     name: "Trinity 2024",
-    thumbnailUrl:
-      "https://ucd3f8aa593a2f95282662037c43.previews.dropboxusercontent.com/p/thumb/ACMIac4Vmgdp08xiRYyiow69vhiYSDJrphec5ktQe28L1ZQgsaypRn03Ke8w_8l9MVYRBPKozd7b2kVYT1LtdHjOseDU1Z_P28v-v5M7vE_EOFD6YoJ4fbqVoBB8B-tM-xbBQ9hc-cHp6Cvs32XSnS7BL_TYYvkeej44Pv_s4inKLRwis_CUD30eAFk0ZbZCX6hI4eNGO5fPilubfNjUrNRgWr5Z8hYNfhZnQqTkWCag9gHKc6CkwqRgx2xRCBB9NSaOl_r6-2SR3Cn2207Zin9AA39zy9ZCB0hK4n4bi-zgpEo7mmM6SfqEVNHlFMixGiQsKH5ZR57950bzQ_x3C52hyfenx9APkE0xpLsdmrcVLBtT2sEZa_-UUWU7x8W85gI/p.png",
+    thumbnailUrl: tr2024,
     concept: ["WebGL", "Web", "Full Stack"],
     projectUrl: "/projects/trinity-2024",
   },
@@ -147,8 +150,7 @@ export const Projects = [
     id: 8,
     idSelector: "particles-gpgpu",
     name: "Particles GPGPU",
-    thumbnailUrl:
-      "https://uc7a6b4e50ec23117fa72e7ae7ea.previews.dropboxusercontent.com/p/thumb/ACOk7Q0Fz-ils_xdzxb9hmhebbRzgdbMCh99z9myX6OOn1TfTfNth6wT5NNKkTTKGIIWHql2j2Gn2-Yh3z9nm83bmvU2RvLiskfYm1m-nl5Qom9yBSNtkACtOk__m2iBPwz6sGdiyvdCCNb1_-kDgAElgTyoIVuyiGuqy03ytNwC914OEmNYoGz__XU_C5RkAF_RaO5QB8_uscnnmXYyKB2FrwkCeI8HXisjC7qusbp2hyloAI6nSbT0NAFN4_ARwpqXvphAolgrsfyWCm-Zi2pXnHEYeSF2ZLrDNO0GyRQHrcS1n53A5xpY8qwTJKoN5XAwazSdjUzGV5SnsprsWbe0asMLjcBnJEbFsZ32hkI8k77FlMzpxoOZbR1tAQKRrRM/p.png",
+    thumbnailUrl: particlesGPGPU,
     concept: ["3D Illustration", "Shaders"],
     projectUrl: "/projects/particles-gpgpu",
   },
@@ -156,8 +158,7 @@ export const Projects = [
     id: 9,
     idSelector: "ironman-holograms",
     name: "Ironman Holograms",
-    thumbnailUrl:
-      "https://uc8fd5ed8ad2e3c359d2808f8b04.previews.dropboxusercontent.com/p/thumb/ACOa8M_b6QSrXSCb17LdonzL9JYeyLhwX8nDAZXSw1U_nEzZ-x3Yzt72mT1jWXCGO6PROasnPdN9ZfEPZLSUHbtnr6X8-rucUIi8lodGyyOiCQx0ZT23OyjgERbsztKV5ZPS-IdLpYsSVqEivXy7dm9f7pbM3qiEta5y0O6dv--IqK4GZSq_VAknM6_rDtKFvox-styu8J47riLKEUa5T6l2fLCnGrXnEcOFW02IhBvYAwSQvQwrCiRg-zONWHC5UfSW7e5Y2WeCwR16t7itk_pdtGXwU5cdK3HM2josjSk3FBjv00gwMK9QZ4G-n-rw2WVmknTL5O1hONsyzvOtcKyopaPjx00VuLcCVlBMSuO0QccB3VP8qDyljU6YgtNW1cY/p.png",
+    thumbnailUrl: ironmanHolograms,
     concept: ["3D Illustration", "Shaders"],
     projectUrl: "/projects/ironman-holograms",
   },

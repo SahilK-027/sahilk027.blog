3b877d5de92219c74014e6247c8d56642b8c8060
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2024-06-16 07:03:08 +0530

    🚫 Block Publisher Route

--- src/App.jsx
+++ src/App.jsx
@@ -216,7 +216,7 @@ const App = () => {
               />
             }
           />
-          <Route path="/publisher" element={<PublisherPage />} />
+          {/* <Route path="/publisher" element={<PublisherPage />} /> */}
           <Route
             path="/*"
             element={
